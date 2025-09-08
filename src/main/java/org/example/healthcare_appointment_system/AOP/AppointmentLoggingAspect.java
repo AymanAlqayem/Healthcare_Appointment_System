982e69d81d430ed6c82669790f45d2afc446d56c
Ayman.Alqayem <nabilayman021@gmai.com>
2025-09-08 14:32:23 +0300

    log event via AOP.

---
+++ src/main/java/org/example/healthcare_appointment_system/AOP/AppointmentLoggingAspect.java
@@ -0,0 +1,31 @@
+package org.example.healthcare_appointment_system.AOP;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class AppointmentLoggingAspect {
+
+    // After booking an appointment successfully
+    @AfterReturning(
+            pointcut = "execution(* org.example.healthcare_appointment_system.service.AppointmentService.bookAppointment(..))",
+            returning = "response"
+    )
+    public void logAppointmentBooking(Object response) {
+        log.info("Appointment booked: {}", response);
+    }
+
+    // After cancelling an appointment successfully
+    @AfterReturning(
+            pointcut = "execution(* org.example.healthcare_appointment_system.service.AppointmentService.cancelAppointment(..))",
+            returning = "response"
+    )
+    public void logAppointmentCancellation(Object response) {
+        log.info("Appointment cancelled: {}", response);
+    }
+}
+
