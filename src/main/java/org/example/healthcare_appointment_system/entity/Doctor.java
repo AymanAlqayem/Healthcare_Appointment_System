9b51fa73b16f67b22620940e132355ab0ff32128
Ayman.Alqayem <nabilayman021@gmai.com>
2025-09-09 16:47:33 +0300

    V1

--- src/main/java/org/example/healthcare_appointment_system/entity/Doctor.java
+++ src/main/java/org/example/healthcare_appointment_system/entity/Doctor.java
@@ -3,6 +3,40 @@ package org.example.healthcare_appointment_system.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import java.util.List;
+
+
+//@Entity
+//@Table(name = "doctors")
+//@Getter
+//@Setter
+//@Cacheable
+//@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+//public class Doctor {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String specialty;
+//
+//    @OneToOne
+//    @JoinColumn(name = "user_id", nullable = false, unique = true)
+//    private User user;
+//
+//    @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Appointment> appointments;
+//
+//    @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<AvailabilitySlot> availabilitySlots;
+//}
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import java.util.List;
 
@@ -10,6 +44,8 @@ import java.util.List;
 @Table(name = "doctors")
 @Getter
 @Setter
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE) // Second-level cache for Doctor
 public class Doctor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,11 +57,15 @@ public class Doctor {
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
+    // Cache collections as well
     @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
     private List<Appointment> appointments;
 
     @OneToMany(mappedBy = "doctor", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
     private List<AvailabilitySlot> availabilitySlots;
 }
+
 
 
